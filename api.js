6f1331b45321eb49e39e687688e875b71f9fa967
script-js <134544514+script-js@users.noreply.github.com>
2024-01-12 17:48:27 -0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,11 @@
+function getLinkWithPopup(url,keyword) {
+  var popup1 = window.open("https://jsurl.pages.dev/add?key=" + keyword + "&url=" + url;
+}
+
+function getLink(url,keyword) {
+  var f = document.createElement("iframe");
+  f.style = "width:0px;border:none;height:0px;";
+  f.src = "https://jsurl.pages.dev/add?key=" + keyword + "&url=" + url;
+  document.body.appendChild(f);
+  return "https://jsurl.pages.dev/" + keyword
+}
