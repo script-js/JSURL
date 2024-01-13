d356759c93b6e446eb69c26b4e8235e10dd723fb
script-js <134544514+script-js@users.noreply.github.com>
2024-01-12 18:30:58 -0800

    Update api.js

--- api.js
+++ api.js
@@ -3,9 +3,9 @@ function getLinkWithPopup(url,keyword) {
 }
 
 function getLink(url,keyword) {
-  var f = document.createElement("iframe");
-  f.style = "width:0px;border:none;height:0px;";
-  f.src = "https://jsurl.pages.dev/add?key=" + keyword + "&url=" + url;
-  document.body.appendChild(f);
+  var jsurlgenerator = document.createElement("iframe");
+  jsurlgenerator.style = "width:0px;border:none;height:0px;";
+  jsurlgenerator.src = "https://jsurl.pages.dev/add?key=" + keyword + "&url=" + url;
+  document.body.appendChild(jsurlgenerator);
   return "https://jsurl.pages.dev/" + keyword
 }
